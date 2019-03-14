1f683642936f7d92d87ebf3ec003ce7fadf19ef6
yaogengzhu <455947455@qq.com>
2019-03-14 22:56:11 +0800

    数据图表使用echart

--- src/components/reports/reporrList.vue
+++ src/components/reports/reporrList.vue
@@ -1,15 +1,59 @@
 <template>
-    <div class="reports">
-        数据列表
-    </div>
+  <div class="reports">数据列表
+    <!-- 存放数据 -->
+    <div class="main" ref="main"></div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+// import echart
+const echarts = require("echarts");
+export default {
+  data() {
+    return {};
+  },
+  // 在chuangjian
+  created() {
+    // 初始化数据，获取数据
+    this.initData();
+    //  初始化dom
+  },
+  mounted() {
+    const myChart = echarts.init(this.$refs.main);
+
+    myChart.setOption({
+      title: {
+        text: "ECharts 入门示例"
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    });
+  },
+  methods: {
+    async initData() {
+      const { data: res } = await this.$http.get("reports/type/1");
+      console.log(res);
     }
+  }
+};
 </script>
-
-<style scoped>
-
+<style lang="scss" scoped>
+.reports {
+  .main {
+    width: 600px;
+    height: 400px;
+    margin-left: 30px;
+    border: 1px solid blue;
+  }
+}
 </style>
